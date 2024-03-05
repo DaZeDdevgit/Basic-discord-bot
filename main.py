a8668fbd492645513ad3c528c905587f0b1225c9
Edgar <68908833+Edgardevgit@users.noreply.github.com>
2024-03-05 16:54:04 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import random
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix="!", case_insensitive=False, intents=intents)
@@ -21,6 +22,14 @@ async def on_ready():
 @bot.tree.command()
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hello {interaction.user.mention}!")
+
+
+@bot.tree.command()
+async def dice(interaction: discord.Interaction):
+    embed = discord.Embed(title=":game_die: Dice", description=f"You rolled the dice and got {random.randint(1,6)}!", color=discord.Color.red()) # You can add Options like RGB and more.
+    embed.set_thumbnail(url="https://clipground.com/images/clipartdice-clipart-9.png")
+    await interaction.response.send_message(embed=embed)
+
 
 
 
